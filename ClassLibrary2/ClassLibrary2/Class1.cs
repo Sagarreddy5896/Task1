60cbca3ccf20d3842072f368ec8d7627647e3b8d
Admin <Admin@DESKTOP-FCNC2RJ>
2022-07-01 11:25:07 +0530

    Add project files.

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public void display()
+        {
+            Console.WriteLine("I am great");
+        
+        }
+
+    }
+
+}
